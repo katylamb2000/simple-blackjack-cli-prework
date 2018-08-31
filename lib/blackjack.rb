33dfa2aa5c7a063046bcd41ebacaadb06bd12691
katy@karstaway.com <katy@karstaway.com>
2018-08-31 20:05:48 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,7 +26,7 @@ def end_game (card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round
+def initial_round (card_total)
   2.times do deal_card
   return card_total
   display_card_total
